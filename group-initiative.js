0609b98f900d3abf827438fcee4b14034ee22422
Toni Fisler <mail@toni.io>
2020-10-06 01:22:09 +0200

    rollback for 0.6

--- group-initiative.js
+++ group-initiative.js
@@ -87,7 +87,7 @@ async function rollGroupInitiative(creatures) {
   };
 
   // Roll initiative for the group leaders only.
-  await this.rollInitiative(ids, {messageOptions});
+  await this.rollInitiative(ids, null, messageOptions);
 
   // Prepare the others in the group.
   const updates = creatures.reduce((updates, {_id, initiative, actor}) => {
